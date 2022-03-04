aca848962c02d785871f493efa38a8a0514d4717
rookieno <rhkdals3257@naver.com>
2022-03-05 01:20:27 +0900

    중간풀이

---
+++ today_algorithm/algorithm22.py
@@ -0,0 +1,29 @@
+def solution(progresses, speeds):
+    answer = []
+    days = 0
+    count = 0
+
+    while len(progresses) != 0:
+        if progresses[0] + days * speeds[0] > 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            days += 1
+    
+    answer.append(count)
+
+    return answer
+
+
+a = [93, 30, 55]
+b = [1, 30, 5]
+
+c = [95, 90, 99, 99, 80, 99]
+d = [1, 1, 1, 1, 1, 1]
+
+print(solution(a,b))
+print(solution(c,d))
