94a04e58775f0e5f53489443051eb959373dc4a1
rookieno <rhkdals3257@naver.com>
2022-03-10 22:22:06 +0900

    dd

--- today_algorithm/algorithm22.py
+++ today_algorithm/algorithm22.py
@@ -1,22 +1,35 @@
+# def solution(progresses, speeds):
+#     answer = []
+#     days = 0
+#     count = 0
+
+#     while len(progresses) != 0:
+#         if progresses[0] + days * speeds[0] > 100:
+#             progresses.pop(0)
+#             speeds.pop(0)
+#             count += 1
+#         else:
+#             if count > 0:
+#                 answer.append(count)
+#                 count = 0
+#             days += 1
+    
+#     answer.append(count)
+
+#     return answer
+
 def solution(progresses, speeds):
     answer = []
     days = 0
-    count = 0
-
-    while len(progresses) != 0:
-        if progresses[0] + days * speeds[0] > 100:
-            progresses.pop(0)
-            speeds.pop(0)
-            count += 1
+    for i in range(len(progresses)):
+        if progresses[i] + days * speeds[i] > 100:
+            answer.append(days)
         else:
-            if count > 0:
-                answer.append(count)
-                count = 0
             days += 1
-    
-    answer.append(count)
-
+        
     return answer
+
+
 
 
 a = [93, 30, 55]
