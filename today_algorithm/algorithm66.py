173f274744e9d1a883227b480ff7215484c6a519
rookieno <rhkdals3257@naver.com>
2022-03-31 23:41:04 +0900

    DFS&BFS

--- today_algorithm/algorithm66.py
+++ today_algorithm/algorithm66.py
@@ -1,8 +1,4 @@
 # 백준 1260번 DFS와 BFS
-from collections import deque
-import sys
-
-n, m, v = map(int, sys.stdin.readline().split())
 
 # graph = [[] for _ in range(n+!)]
 # for i in range(m):
@@ -25,21 +21,57 @@ n, m, v = map(int, sys.stdin.readline().split())
 
 # print(graph) [[], [2, 3, 4], [1, 4], [1, 4], [1, 2, 3]]
 
-graph = [[0]*(n+1) for _ in range(n+1)]
+# graph = [[0]*(n+1) for _ in range(n+1)]
+
+# for i in range(m):
+#     a ,b = map(int, sys.stdin.readline().split())
+#     graph[a][b] = graph[b][a] = 1
+
+# #print(graph) [[0, 0, 0, 0, 0], [0, 0, 1, 1, 1], [0, 1, 0, 0, 1], [0, 1, 0, 0, 1], [0, 1, 1, 1, 0]]
+
+# def DFS(adjacent_graph, start_node):
+#     visited += [start_node]
+#     for i in range(len(adjacent_graph[start_node])):
+#         if adjacent_graph[start_node][i] == 1 and i not in visited:
+#             DFS(i, visited)
+#     return visited
+
+
+def DFS(start_node):
+    print(start_node, end=' ')
+    visited[start_node] = True
+    for i in graph[start_node]:
+        if not visited[i]:
+            DFS(i)
+
+
+def BFS(start_node):
+    visited[start_node] = True
+    que = deque([start_node])
+    while que:
+        cur_node = que.popleft()
+        print(cur_node, end=' ')
+        for i in graph[cur_node]:
+            if not visited[i]:
+                que.append(i)
+                visited[i] = True
+
+from collections import deque
+import sys
+
+n, m, v = map(int, sys.stdin.readline().split())
+
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
 
 for i in range(m):
-    a ,b = map(int, sys.stdin.readline().split())
-    graph[a][b] = graph[b][a] = 1
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-#print(graph) [[0, 0, 0, 0, 0], [0, 0, 1, 1, 1], [0, 1, 0, 0, 1], [0, 1, 0, 0, 1], [0, 1, 1, 1, 0]]
-
-def DFS(adjacent_graph, start_node):
-    visited += [start_node]
-    for i in range(len(adjacent_graph[start_node])):
-        if adjacent_graph[start_node][i] == 1 and i not in visited:
-            DFS(i, visited)
-    return visited
-
-
+for i in range(1, n+1):
+    graph[i].sort()
     
-print(DFS(graph, v))
+DFS(v)
+visited = [False] * (n+1)
+BFS(v)
