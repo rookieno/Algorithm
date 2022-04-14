e8949de5b5757eb8478ee9470640d266a99d5642
rookieno <rhkdals3257@naver.com>
2022-04-14 21:00:29 +0900

    2022.04.14

--- today_algorithm/algorithm73.py
+++ today_algorithm/algorithm73.py
@@ -1,14 +1,26 @@
 # 백준 2579번 계단오르기
+import sys
+
+input = sys.stdin.readline
+
 n = int(input())
 
-steps = []
-dp = []
+steps = [0] * 301
+dp = [0] * 301
 
 for i in range(n):
-    step = int(input())
-    steps.append(step)
+    steps[i] = int(input())
 
-dp.append(steps[0])
+if n == 1:
+    print(steps[0])
+elif n == 2:
+    print(steps[0] + steps[1])
+else:
+    dp[0] = steps[0]
+    dp[1] = steps[0] + steps[1]
+    dp[2] = max(steps[1]+steps[2], steps[0]+steps[2])
 
-def dp():
-    return
+    for i in range(3, n):
+        dp[i] = max(dp[i-3]+steps[i]+steps[i-1], dp[i-2]+steps[i])
+    
+print(dp[n-1])
