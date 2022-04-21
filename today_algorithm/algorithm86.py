99063233eb2123405372f33e4054148050f47409
rookieno <rhkdals3257@naver.com>
2022-04-21 21:46:19 +0900

    2022.04.21

--- today_algorithm/algorithm86.py
+++ today_algorithm/algorithm86.py
@@ -1,6 +1,15 @@
 # 백준 7568번 덩치
+from pyrsistent import b
+
+
 n = int(input())
+
+answer = []
+
+big = []
 
 for i in range(n):
     x, y = map(int, input().split())
-    
+    big.append((x,y))
+
+print(big)
