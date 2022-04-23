18d5358db58f78cc326250869fba206cb3398c96
rookieno <rhkdals3257@naver.com>
2022-04-23 21:25:51 +0900

    2022.04.23

--- today_algorithm/algorithm86.py
+++ today_algorithm/algorithm86.py
@@ -1,11 +1,19 @@
 # 백준 7568번 덩치
 n = int(input())
 
-answer = []
+size = []
 
-big = []
+answer = []
 
 for i in range(n):
     x, y = map(int, input().split())
-    big.append((x,y))
-    
+    size.append((x,y))
+
+for i in size:
+    rank = 1
+    for j in size:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    answer.append(rank)
+
+print(' '.join(list(map(str, answer))))
