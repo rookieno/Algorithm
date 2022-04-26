b40ffe5b7ecaad74da097652ba00b2dfe0d1507d
rookieno <rhkdals3257@naver.com>
2022-04-26 22:15:55 +0900

    2022.04.26

--- today_algorithm/algorithm87.py
+++ today_algorithm/algorithm87.py
@@ -4,18 +4,21 @@ n ,m = map(int, input().split())
 chess = []
 
 for i in range(n):
-    line = str(input())
-    chess.append(line)
+    line = list(str(input()))
+    chess.append(line[:8])
 
 check = 0
 
 for idx,i in enumerate(chess):
-    for j in range(m-2):
+    for j in range(7):
         if i[j] == i[j+1]:
             if i[j] == 'W':
                 chess[idx][j+1] = 'B'
             else:
                 chess[idx][j+1] = 'W'
             check += 1
+
+# for i in chess:
+    
 
 print(check)
