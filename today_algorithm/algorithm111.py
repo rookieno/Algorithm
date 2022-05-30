75e77a16ab76171e702379f20ac193ed9c7c2549
rookieno <rhkdals3257@naver.com>
2022-05-31 00:40:04 +0900

    11051

--- today_algorithm/algorithm111.py
+++ today_algorithm/algorithm111.py
@@ -8,5 +8,19 @@ dp = [[1 for _ in range(k+1)] for _ in range(n+1)]
 for i in range(1, k+1):
      for j in range(i+1, n+1):
         dp[j][i] = (dp[j-1][i-1] + dp[j-1][i]) % 10007 
-        
+
 print(dp[n][k])
+
+top = list(set(range(n+1)) - set(range(k+1)))
+bot = list(range(1, n-k+1))
+
+a = 1
+b = 1
+
+for i in top:
+   a *= i
+
+for i in bot:
+    b *= i
+
+print(a//b % 10007)
